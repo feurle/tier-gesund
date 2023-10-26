0c99cfceeb8f3f5af4b5f8accc81fb444d7064aa
Daniel Feurle <daniel@feurle.com>
2023-10-26 16:12:20 +0200

    first commit

---
+++ src/main/java/com/feurle/tiergesund/TierGesundApplication.java
@@ -0,0 +1,13 @@
+package com.feurle.tiergesund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierGesundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierGesundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feurle/tiergesund/TierGesundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feurle.tiergesund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierGesundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
