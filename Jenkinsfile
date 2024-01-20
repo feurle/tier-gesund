607516014251cc196fcd470f551c31abf831f756
Daniel Feurle <daniel@feurle.com>
2024-01-20 22:40:32 +0100

    new change

--- Jenkinsfile
+++ Jenkinsfile
@@ -32,7 +32,7 @@ pipeline {
     }
     stage('Deploy Artefact') {
       steps {
-
+        echo '======================='
       }
     }
   }
--- src/main/java/com/feurle/tiergesund/TierGesundApplication.java
+++ src/main/java/com/feurle/tiergesund/TierGesundApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TierGesundApplication {
+public class  TierGesundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TierGesundApplication.class, args);
