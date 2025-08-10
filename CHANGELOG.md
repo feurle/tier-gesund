aa33fe7d59fbf499ca5dbee97f9e228b76b751d5
Daniel Feurle <dfeurle@yandex.ru>
2025-08-10 04:18:35 +0200

    Fix hard wired entity loading from home

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,5 @@
 
 ##### Other Changes
 
-- feurle/tier-gesund (f4a045f8)
-- feurle/tier-gesund (0a6e8e2f)
+- feurle/tier-gesund ([f4a045f8](https://github.com/feurle/tier-gesund/commit/f4a045f880e4029d16a0f1cdbe9772477218464f))
+- feurle/tier-gesund ([0a6e8e2f](https://github.com/feurle/tier-gesund/commit/0a6e8e2f7064d46b414054ed247e094d2297ec57))
--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -8,7 +8,7 @@ import { Alert, Col, Row } from 'reactstrap';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 
-import { getEntity, getHomeEntities } from 'app/entities/news-article/news-article.reducer';
+import { getHomeEntities } from 'app/entities/news-article/news-article.reducer';
 
 export const Home = () => {
   const account = useAppSelector(state => state.authentication.account);
@@ -24,11 +24,9 @@ export const Home = () => {
 
   React.useEffect(() => {
     // Replace '1' with the ID you want to fetch
-    dispatch(getEntity(1));
     getAllEntities();
   }, []);
 
-  const newsArticleEntity = useAppSelector(state => state.newsArticle.entity);
   const newsArticleList = useAppSelector(state => state.newsArticle.entities);
 
   if (loading) {
@@ -69,37 +67,6 @@ export const Home = () => {
             </div>
           </Alert>
         </div>
-        {account?.login ? (
-          <div>
-            <Alert color="success">
-              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
         <div></div>
         <div>
           {newsArticleList && newsArticleList.length > 0 ? (
